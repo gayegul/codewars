05a1c194381fd079ce7abf0f1bbae211b690dd81
Gaye Bulut <gayegul@gmail.com>
2015-04-24 22:33:51 -0700

    kata6 secret message working

--- 6/secretMessage.js
+++ 6/secretMessage.js
@@ -1,13 +1,20 @@
 function findSecretMessage(paragraph) {
   var seenWords = {};
-  var words = paragraph.toLowerCase().split(/[. ]/);
+  var words = paragraph.toLowerCase().replace(/\W+/g,' ').split(/\s+/);
   var result = "";
+  var space = "";
+
   for(var i = 0; i < words.length; i++) {
-    if(seenWords[words[i]]) {
-      result += " " + words[i];
+    if (!seenWords[words[i]] && seenWords[words[i]] !== "") {
+      seenWords[words[i]] = 1;
+    } else if(seenWords[words[i]] === 1) {
+      result += space + words[i];
+      space = " ";
+      seenWords[words[i]]++;
     } else {
-      seenWords[words[i]] = words[i];
+      seenWords[words[i]]++;
     }
   }
+  
   return result;
 }
