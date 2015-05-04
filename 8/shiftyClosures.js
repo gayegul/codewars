f69a67015b8a758028d04e7deeef3821f1476405
Gaye Bulut <gayegul@gmail.com>
2015-05-04 15:25:50 -0700

    add solution

---
+++ 8/shiftyClosures.js
@@ -0,0 +1,13 @@
+var name;
+
+var greet_abe = function() {
+  name = 'Abe';
+  return "Hello, " + name + '!';
+};
+
+var greet_ben = function() {
+  name = 'Ben';
+  return "Hello, " + name + '!';
+};
+
+greet_abe();
