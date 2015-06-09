d4bc9aae956bd0a96d8ce124920d38618cc02796
Gaye Bulut <gayegul@gmail.com>
2015-06-08 21:19:38 -0700

    minor fix

--- 7/convertingIntegerToCurrency.js
+++ 7/convertingIntegerToCurrency.js
@@ -1,6 +1,5 @@
 function toCurrency(price) {
   price = price.toString().split("");
-  console.log(price);
   var counter = 0;
   for(var i = price.length - 1; i >= 0; i--) {
     if(counter !== 0 && counter % 3 === 0) {
