c9c999ff2a74248fb50090ae9d450982be8ce693
Gaye Bulut <gayegul@gmail.com>
2015-07-24 13:47:04 -0700

    add function description

--- 6/rotateArray.js
+++ 6/rotateArray.js
@@ -1,3 +1,10 @@
+/* Create a function named "rotate" that takes an array and
+returns a new one with the elements inside rotated n spaces.
+If n is greater than 0 it should rotate the array to the right.
+If n is less than 0 it should rotate the array to the left.
+If n is 0, then it should return the array unchanged.
+*/
+
 function rotate(array,n) {
   var array2 = [];
   var length = array.length
