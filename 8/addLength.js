87fbd3e0ecc7bb50cda0a499b37e1d75a1b0c5d2
Gaye Bulut <gayegul@gmail.com>
2015-08-14 04:42:41 -0700

    fix syntax

--- 8/addLength.js
+++ 8/addLength.js
@@ -1,5 +1,5 @@
-/*  a function that takes a String and returns an
-Array/list with the length of each word added to each element */
+/*  a function that takes a string and returns an array/list
+ with the length of each word added to each element */
 
 function addLength(str) {
   var result = [];
